dfe284fa34a6235604735973d1464c60aacf4160
ruiban <13535028665@163.com>
2021-05-31 14:58:29 +0800

    策划新建表单组件拆分

---
+++ src/components/PlanningCreateMain.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="planningCreateMain">
+    <el-button type="primary" @click="handleSave">保存</el-button>
+    <el-form
+      ref="form1"
+      :model="form1"
+      :rules="rules1"
+      label-width="80px"
+      size="small"
+    >
+        <el-form-item label="姓名" prop="name">
+            <el-input v-model="form1.name"></el-input>
+        </el-form-item>
+    </el-form>
+    <el-form ref="form2" :model="form2" :rules="rules2" label-width="80px" size="small">
+        <el-form-item label="公司" prop="company">
+            <el-input v-model="form2.company"></el-input>
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form1: {
+                name: '',
+                age: ''
+            },
+            form2: {
+                company: ''
+            },
+            rules1: {
+                name: [
+                    {required: true, message: '请输入姓名', trigger: 'blur'}
+                ]
+            }
+        }
+    }
+};
+</script>
+<style scoped>
+</style>
